b1427e82b18dcef64f7f02aa90b14b0249fcdf72
MauTol16 <mauriciomtzt@hotmail.com>
2020-11-13 12:15:26 -0600

    main

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  char inputPrimeraLetra;
+  
+  do{
+  cin>>inputPrimeraLetra;
+  inputPrimeraLetra=toupper(inputPrimeraLetra);
+  
+  switch(inputPrimeraLetra)
+  {
+    case 'P':
+      break;
+    case 'A':
+      break;
+    case 'L':
+      break;
+    case 'C':
+      break;
+    case 'F':
+      break;
+    case 'E':
+      break;
+  }
+
+
+  }while(inputPrimeraLetra!='E');
+
+}
